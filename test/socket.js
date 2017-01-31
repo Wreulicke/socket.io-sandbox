64f4e14ad75c4171ab548265960665bb1e54673f
Wreulicke <masaya2dies@gmail.com>
2017-01-31 21:44:14 +0900

    add test

--- test/socket.js
+++ test/socket.js
@@ -16,6 +16,19 @@ test("socket io encode", t => {
   })
 })
 
+test("socket io encode", t => {
+  const packet = {
+    type: parser.BINARY_EVENT,
+    nsp: "/xxx/vvv",
+    data: new Buffer("zzzccccddd"),
+    id: 13
+  }
+  encoder.encode(packet, function(encodedPackets) {
+    t.is(encodedPackets[0], `51-/xxx/vvv,13{"_placeholder":true,"num":0}`)
+    t.is(encodedPackets[1].toString(), "zzzccccddd")
+  })
+})
+
 test("socket io decode", t =>{
   const encodedPackets=[`2/xxx/vvv,13{"x":"test-packet"}`]
   const decoder = new parser.Decoder();
